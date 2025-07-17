c5a9ac3acb61d16d340c504bf332e8a2f2892083
tlspuffin <tlspuffin@tlspuffin>
2025-07-17 15:12:34 +0000

    deploy from tlspuffin/tlspuffin/refs/heads/main@2ed7077aa2a3b937dcbb18d5fc80797bc43f0337

---
+++ website/api/puffin/fuzzer/feedback/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["FAIL_AT_STEP"],"struct":["MinimizingFeedback"]};
---
+++ website/api/trait.impl/libafl/feedbacks/trait.Feedback.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["puffin",[["impl&lt;SC, PT&gt; Feedback&lt;SC&gt; for <a class=\"struct\" href=\"puffin/fuzzer/feedback/struct.MinimizingFeedback.html\" title=\"struct puffin::fuzzer::feedback::MinimizingFeedback\">MinimizingFeedback</a>&lt;SC, PT&gt;<div class=\"where\">where\n    SC: HasCorpus + HasRand + HasMaxSize + UsesInput&lt;Input = <a class=\"struct\" href=\"puffin/trace/struct.Trace.html\" title=\"struct puffin::trace::Trace\">Trace</a>&lt;PT&gt;&gt; + State,\n    PT: <a class=\"trait\" href=\"puffin/protocol/trait.ProtocolTypes.html\" title=\"trait puffin::protocol::ProtocolTypes\">ProtocolTypes</a> + 'static,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[623]}
